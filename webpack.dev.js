e021588ec90d969a989de32def613920d64c5ce2
Bryan <bryanwagoki2016@gmail.com>
2025-12-29 19:10:02 +0300

    Add configuration for dev build

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "development",
+
+  devtool: "eval-source-map",
+
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    open: true,
+    hot: true,
+  },
+});
