6f44c7686d7935534c8ef292f8223f759bdcd131
Bryan <bryanwagoki2016@gmail.com>
2025-12-29 19:37:51 +0300

    Add the relevant webpack packages and update HTML and CSS files

--- src/index.js
+++ src/index.js
@@ -8,14 +8,3 @@ console.log("wuuuiiii")
 
 
    
-const image = document.createElement("img");
-image.src = odinImage;
-// image.display = "block"
-   
-document.body.appendChild(image);
-
-const image2 = document.createElement("img");
-image2.src = arooo;
-// image2.display = "block"
-   
-document.body.appendChild(image2);
